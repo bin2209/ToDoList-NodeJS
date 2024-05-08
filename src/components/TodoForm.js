3718da2e43a0d2f3c99b688c662e10d40caafab5
bin2209 <binazure@icloud.com>
2024-05-08 07:30:00 +0700

    Setup NodeJS and Build complete can start npm

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("")
+    const handleSubmit = e =>{
+        e.preventDefault();
+        addTodo(value)
+        setValue("")
+    }
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' placeholder='What is the task to day?'
+        value = {value} 
+        onChange={(e) => setValue(e.target.value)}/>
+
+        <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
+  )
+}
